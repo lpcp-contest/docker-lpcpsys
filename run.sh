2dc27813864cb17526bf2f580c013e2054d7a952
Jose F. Morales <jfmcjf@gmail.com>
2019-08-23 15:00:05 +0200

    mount pwd in docker run

--- run.sh
+++ run.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+# Physical directory where the script is located
+_base=$(e=$0;while test -L "$e";do d=$(dirname "$e");e=$(readlink "$e");\
+        cd "$d";done;cd "$(dirname "$e")";pwd -P)
+
 sys=$1
 shift
 
@@ -9,4 +13,6 @@ if [ -t 0 ]; then # input is a tty
 fi
 
 # TODO: share current dir so that we can pass files as args
-docker run --rm $opts -v "$(pwd)"/../2016_samples:/data lpcpsys:latest /opt/run-$sys.sh "$@"
+#docker run --rm $opts -v "$(pwd)"/../2016_samples:/data lpcpsys:latest /opt/run-$sys.sh "$@"
+docker run --rm $opts -v "$(pwd)":/data lpcpsys:latest /opt/run-$sys.sh "$@"
+
