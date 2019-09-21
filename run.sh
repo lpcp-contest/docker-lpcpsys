448687bb6628b9afebb6fa2e1457ca2d6e78d260
Jose F. Morales <jfmcjf@gmail.com>
2019-09-21 16:28:49 -0600

    Workaround slow mounted volumes on macOS systems

--- run.sh
+++ run.sh
@@ -24,5 +24,23 @@ else
     runtimeout="timeout ${TIMEOUT}"
 fi
 
+# (workaround very slow volume mounts in macOS)
 # Note: using 'delegated' mounted volume, for performance
-docker run --rm $opts -v "$(pwd)":/data:delegated lpcpsys:latest $runtimeout /opt/run-$sys.sh "$@"
+if [ -r runaux.sh ]; then
+    cat <<EOF
+ERROR: Conflict, runaux.sh already exists
+EOF
+    exit 1
+fi
+cat > runaux.sh <<EOF
+#!/bin/sh
+mkdir -p /data
+cp /data0/* /data
+cd /data    
+$runtimeout /opt/run-$sys.sh \$@
+EOF
+chmod a+x runaux.sh
+docker run --rm $opts -v "$(pwd)":/data0 lpcpsys:latest /data0/runaux.sh "$@"
+rm runaux.sh
+
+# docker run --rm $opts -v "$(pwd)":/data:cached lpcpsys:latest $runtimeout /opt/run-$sys.sh "$@"
